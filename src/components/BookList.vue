a64e59f44d1c766fa703e222e97bc1515e3ba451
Rehan Rabbani <rrabbani@nisum.com>
2020-10-27 12:55:12 +0500

    check template

---
+++ src/components/BookList.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script>
+export default {
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+    };
+  },
+};
+</script>
+<style lang="stylus" scoped></style>
